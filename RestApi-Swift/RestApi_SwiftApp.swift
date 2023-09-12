45166ac75c270d0d805943a3388a65ef5bc87b03
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-09-12 14:34:37 +0200

    Initial Commit

---
+++ RestApi-Swift/RestApi_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestApi_SwiftApp.swift
+//  RestApi-Swift
+//
+//  Created by Farnaz Rismanchian on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestApi_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
